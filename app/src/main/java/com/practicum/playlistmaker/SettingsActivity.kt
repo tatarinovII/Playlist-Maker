42e04989f4e136bce1bbdf9fc271b6bafed2bb5b
yaBabyIlyha <ilya.tatarinov77@gmail.com>
2025-08-12 20:19:40 +0300

    first check
    check5

---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,21 @@
+package com.practicum.playlistmaker
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        val bBack = findViewById<ImageView>(R.id.bBack)
+
+        bBack.setOnClickListener {
+            val intent = Intent(this@SettingsActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
